cdfda1151d0f6e391ffd2d1b062ae02fd012d07e
Bharath Kumar <kumar7bharath@gmail.com>
2023-03-23 18:11:46 -0400

    simulation working

--- moveit_adapter/src/moveit_adapter_module/grasping.py
+++ moveit_adapter/src/moveit_adapter_module/grasping.py
@@ -7,10 +7,10 @@ class Gripper:
         self.sim_mode = sim_mode
         
         if self.sim_mode:
-            self.finger1_pub = rospy.Publisher('/panda_finger1_controller/command', Float64, queue_size=10)
-            self.finger2_pub = rospy.Publisher('/panda_finger2_controller/command', Float64, queue_size=10)
+            self.finger1_pub = rospy.Publisher('/panda_finger1_controller/command', Float64, queue_size=1)
+            self.finger2_pub = rospy.Publisher('/panda_finger2_controller/command', Float64, queue_size=1)
         else:
-            self.grasp_pub = rospy.Publisher('/franka_gripper/grasp/goal', GraspActionGoal, queue_size=10) 
+            self.grasp_pub = rospy.Publisher('/franka_gripper/grasp/goal', GraspActionGoal, queue_size=1) 
         rospy.sleep(1)
         
     def grasp(self, width):
